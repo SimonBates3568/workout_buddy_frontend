e636b9767c38dc20a163f7b1a9868ce34e37e7ac
simon bates <simonbates52@gmail.com>
2025-09-16 11:57:22 +0200

    ADD CSS

--- src/App.js
+++ src/App.js
@@ -12,19 +12,21 @@ import Footer from './components/Footer';
 function App() {
   const { user } = useAuthContext();
   return (
-    <div className="App">
+    <div className="app-wrapper">
       <BrowserRouter>
         <Navbar />
-        <div className='pages'>
-          <Routes>
-            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
-          </Routes>
-          <Routes>
-            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-          </Routes>
-          <Routes>
-            <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
-          </Routes>
+        <div className="main-content">
+          <div className='pages'>
+            <Routes>
+              <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            </Routes>
+            <Routes>
+              <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+            </Routes>
+            <Routes>
+              <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
+            </Routes>
+          </div>
         </div>
         <Footer />
       </BrowserRouter>
